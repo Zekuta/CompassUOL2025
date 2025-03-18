5cfc1d92f25b10e5523c08d3a510b6916ecdd483
Bruno Viera <109918268+Zekuta@users.noreply.github.com>
2025-03-18 10:21:01 -0300

    Update activity_1.js
    aditional user input to array

--- activity_1.js
+++ activity_1.js
@@ -1,12 +1,13 @@
-let array = [1, 2, 3, 4, 5, 6, 7];
+const input = prompt("Ingresa un array de números separados por comas: ");
+const array = input.split(",");
+// array = [1, 2, 3, 4, 5, 6, 7];
+
 console.log(ArraySoma(array));
 
 function ArraySoma(array){
-    print("Informe sus ")
-
     let soma = 0;
     for(let i=0;i<array.length;i++){
-        soma = array[i] + soma;
+        soma = soma + Number(array[i]);
     }
 return soma;
 }

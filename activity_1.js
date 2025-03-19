6ef5a233fb0f51a6931987b86956420542246a34
Bruno Viera <109918268+Zekuta@users.noreply.github.com>
2025-03-19 10:57:28 -0300

    Update activity_1.js
    added check for verification of strings.

--- activity_1.js
+++ activity_1.js
@@ -1,5 +1,6 @@
 const input = prompt("Ingresa un array de números separados por comas: ");
 const array = input.split(",");
+let check = true;
 // array = [1, 2, 3, 4, 5, 6, 7];
 
 console.log(ArraySoma(array));
@@ -7,7 +8,17 @@ console.log(ArraySoma(array));
 function ArraySoma(array){
     let soma = 0;
     for(let i=0;i<array.length;i++){
-        soma = soma + Number(array[i]);
+        if(isNaN(array[i])){
+          console.log("La variable referenciada no es un numero.");
+          check = false;
+          break;
+        } else {
+          soma = soma + Number(array[i]);
+        }
     }
-return soma;
+    if(check === false){
+      console.log("Para recibir la suma, debe reiniciar la app.")
+    } else {
+      console.log("La suma es: " + soma);
+    }
 }
